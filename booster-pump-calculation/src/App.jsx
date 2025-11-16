2946950dbb9f4b88ef1d0bf8b05d5e90c86cedee
engissa98 <m.issa.apex98@gmail.com>
2025-11-16 14:26:04 +0300

    Add files via upload

---
+++ booster-pump-calculation/src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ padding: 40, color: 'white', background: '#0d1117', minHeight: '100vh' }}>
+      <h1>Booster Pump Tool</h1>
+      <p>Your full UI will be plugged here.</p>
+    </div>
+  );
+}
