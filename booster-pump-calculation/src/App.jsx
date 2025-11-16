053673954be9ebc2e34e0526b813f42e2d313776
engissa98 <m.issa.apex98@gmail.com>
2025-11-16 14:30:51 +0300

    Delete booster-pump-calculation directory

--- booster-pump-calculation/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function App() {
-  return (
-    <div style={{ padding: 40, color: 'white', background: '#0d1117', minHeight: '100vh' }}>
-      <h1>Booster Pump Tool</h1>
-      <p>Your full UI will be plugged here.</p>
-    </div>
-  );
-}
